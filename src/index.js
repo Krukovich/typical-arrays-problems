65d1e8f1d0f9939eebf5a381bd1067d3afe71022
Alexey <Shaht3r@gmail.com>
2020-02-23 00:52:34 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+	if (arguments.length === 0 || array.length === 0) return 0;
+
+	const prepareArray = array.sort(sortArray);
+	return prepareArray[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+	if (arguments.length === 0 || array.length === 0) return 0;
+
+	const prepareArray = array.sort(sortArray);
+	return prepareArray[prepareArray.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if (arguments.length === 0 || array.length === 0) return 0;
+
+	const sumArrayNumber = array.reduce((a, b) => a + b);
+	return sumArrayNumber / array.length;
+}
+
+function sortArray(a, b) {
+	return a - b;
 }
